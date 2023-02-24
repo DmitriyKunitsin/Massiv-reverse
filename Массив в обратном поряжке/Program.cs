af4e3244111931ad172c5ca3fbf7a171975639b4
kunicind1996@gmail.com <kunicind1996@gmail.com>
2023-02-24 22:55:53 +0700

    Добавьте файлы проекта.

---
+++ Массив в обратном поряжке/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Введите колличество элементов массива: \t");
+int NumberArray = int.Parse(Console.ReadLine());
+int[] NewArray = new int[NumberArray];
+for (int i = 0; i < NewArray.Length; i++)
+{
+    Console.Write($"Вы вводите значение массива {i}: \t");
+    NewArray[i] = int.Parse(Console.ReadLine());
+}
+Console.WriteLine("\nВывод массива : \t");
+for (int i = NewArray.Length-1; i >= 0; --i)
+{
+    Console.WriteLine(NewArray[i]);
+}
+Console.ReadLine();
